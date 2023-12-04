360b2334c40d8127be59b72a61be51a8f4491cd0
Sam Parkinson <43306113+sam-parkinson@users.noreply.github.com>
2023-12-04 13:25:52 -0500

    Update gitignore

---
+++ SwedishDancePop.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class SwedishDancePop {
+    private String[][] addresses;
+
+    public SwedishDancePop(String address) {
+        
+    }
+
+    private void parseInput(String address) {
+        
+    } 
+}
